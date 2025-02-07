a71c5f433d40b64258471b1e2043b181359d3011
Temesgen Mehari <temesgen.mehari@hhi.fraunhofer.de>
2025-02-07 15:37:41 +0100

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'serif': ['"Computer Modern Serif"', 'serif'],
+      },
+    },
+  },
+} 
